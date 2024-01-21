578ac350ddf948dbb79c23d1048840d5166c8451
Shin'ya Ueoka <ueokande@i-beam.org>
2024-01-21 13:09:31 +0000

    use jest package

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,10 @@
-import type { Config } from "@jest/types";
+import type { Config } from "jest";
 
-const config: Config.InitialOptions = {
+const config: Config = {
   verbose: true,
   testMatch: ["**/tests/**/*.test.+(ts|tsx|js|jsx)"],
   preset: "ts-jest",
   testTimeout: 10000,
 };
+
 export default config;
