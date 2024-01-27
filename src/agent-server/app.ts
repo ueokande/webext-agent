9596f5686bcf72c62057c6acffb05776399589e4
Shin'ya Ueoka <ueokande@i-beam.org>
2024-01-27 02:44:25 +0000

    register endpoint on server

--- src/agent-server/app.ts
+++ src/agent-server/app.ts
@@ -1,29 +1,81 @@
 import type { Readable, Writable } from "node:stream";
 import { start as startServer } from "./server";
 import { RPCClientImpl } from "./rpc";
-import { type Logger } from "pino";
+import pino, { type Logger } from "pino";
+import { LogDiscovery } from "../discovery/logs";
+import { EndpointDiscovery } from "../discovery/endpoints";
 
 type Options = {
-  port: number;
+  addonId: string;
+  port?: number;
+  dataDir: string;
   stdin: Readable;
   stdout: Writable;
-  logger: Logger;
+};
+
+const scheduleGracefulShutdown = (
+  server: { close: () => Promise<void> },
+  logger: Logger,
+) => {
+  process.on("SIGINT", async () => {
+    setTimeout(() => {
+      logger.error("Could not close server gracefully");
+      process.exit(1);
+    }, 3000);
+    await server.close();
+    process.exit();
+  });
 };
 
 export class Application {
   private readonly opts: Options;
   private readonly rpc: RPCClientImpl;
+  private readonly logDiscovery: LogDiscovery;
+  private readonly endpointDiscovery: EndpointDiscovery;
 
   constructor(opts: Options) {
+    this.logDiscovery = new LogDiscovery(opts.dataDir);
+    this.endpointDiscovery = new EndpointDiscovery(opts.dataDir);
     this.opts = opts;
     this.rpc = new RPCClientImpl(opts.stdin, opts.stdout);
   }
 
-  run() {
+  async run() {
+    const logFile = await this.logDiscovery.touchLog(this.opts.addonId);
+    const logger = pino(
+      { timestamp: pino.stdTimeFunctions.isoTime },
+      pino.destination(logFile),
+    );
     this.rpc.start();
     this.rpc.on("error", (err: Error) => {
-      this.opts.logger.error(err);
+      logger.error(err);
     });
-    startServer(this.opts.port, this.rpc, this.opts.logger);
+    const server = await startServer(this.opts.port, this.rpc, logger);
+    server.addHook("onListen", async () => {
+      const addresses = server
+        .addresses()
+        .filter((addr) => addr.family === "IPv4");
+      if (addresses.length === 0) {
+        throw new Error("Server did not listen with IPv4");
+      }
+      await this.endpointDiscovery.installAddon(
+        this.opts.addonId,
+        addresses[0],
+      );
+    });
+    server.addHook("onClose", async () => {
+      await this.endpointDiscovery.uninstallAddon(this.opts.addonId);
+    });
+
+    scheduleGracefulShutdown(server, logger);
+
+    process.stderr.write(
+      `This application is a agent server for webext-agent and does not expect run manually
+You can see server logs at the following path:
+
+  ${logFile}
+
+`,
+    );
   }
 }
--- src/agent-server/server.ts
+++ src/agent-server/server.ts
@@ -4,15 +4,29 @@ import { newRoute as newApiRoute } from "./routeApi";
 import { newRoute as newHealthRoute } from "./routeHealth";
 import { type RPCClient } from "./rpc";
 
-const start = (port: number, rcp: RPCClient, logger: Logger) => {
+const start = async (
+  port: number | undefined,
+  rcp: RPCClient,
+  logger: Logger,
+) => {
   const fastify = Fastify({ logger });
   fastify.register(
-    newApiRoute((method: string, args: unknown[]) => {
-      return rcp.request({ method, args });
+    newApiRoute(async (method: string, args: unknown[]) => {
+      logger.warn({ method, args }, "request");
+      try {
+        const resp = await rcp.request({ method, args });
+        logger.warn({ resp: resp }, "response");
+        return resp;
+      } catch (e) {
+        logger.error({ error: e, errstring: String(e) }, "error");
+        throw e;
+      }
     }),
   );
   fastify.register(newHealthRoute());
   fastify.listen({ port });
+
+  return fastify;
 };
 
 export { start };
